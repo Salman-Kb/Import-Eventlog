a7abcc3f8e9d6e8d9fb65fcf2854630e1616df09
Salman-Kb <137834927+Salman-Kb@users.noreply.github.com>
2023-07-09 13:20:33 +0400

    Add files via upload

---
+++ GetEventLog.ps1
@@ -0,0 +1,6 @@
+﻿$Date1 = Get-Date 10/25/2022
+$Date2 = Get-Date 11/15/2022
+$Computer = Get-Content "E:\list.txt"
+Foreach ($Computer in $Computer){
+    Get-winEvent -ComputerName $Computer -FilterHashtable @{logName='Security'; StartTime=$Date1; Endtime=$Date2} | Export-CSV "E:\Excel\SecurityEventLogs\$Computer.csv" –NoTypeInformation
+    }
